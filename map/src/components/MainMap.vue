09da75c477a09e0ac6751488c82eaf9ea949641b
RoyHong2 <dphong88223@gmail.com>
2022-09-03 00:48:49 +0900

    plz branch

--- map/src/components/MainMap.vue
+++ map/src/components/MainMap.vue
@@ -22,6 +22,7 @@
         //2nd test
         //roy branch에서 수정 후 commit
         //roy branch push test2
+        //branch sep
       this.OlMap = new OlMap({
         target: this.$refs.map,
         layers: [
